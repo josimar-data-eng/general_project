9ced232352643caab40dff4d9857d9395f96eb19
Josimar Junior <josimar_junior@id.uff.br>
2022-12-16 13:53:34 -0300

    airflow pipeline to run PySpark job

---
+++ airflow/create_composer.sh
@@ -0,0 +1,3 @@
+gcloud composer environments create airflow-jobs \
+--location=us-central1 \
+--service-account=general-projects-675@dev-project-363923.iam.gserviceaccount.com
---
+++ airflow/send_dag_file.sh
@@ -0,0 +1,1 @@
+gsutil cp firts_dag.py gs://us-central1-airflow-jobs-69c328fe-bucket/dags
